a2647663a3d1e6d897d313d88ddf58f182c926e9
pkkwilliam <pkkwilliam919@gmail.com>
2021-05-04 10:58:14 +0800

    fix minor

--- src/common/applicationContext.js
+++ src/common/applicationContext.js
@@ -1,8 +1,8 @@
 export default class ApplicationContext {
   get host() {
     return this.mock
-      ? "http://192.168.31.164:8081/"
-      : "http://192.168.31.164:8081/";
+      ? "http://192.168.31.164:8081"
+      : "http://192.168.31.164:8081";
   }
 
   get mock() {
